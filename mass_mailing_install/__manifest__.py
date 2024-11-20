d42d171316840ff80ebf06cf784a084689d2b149
tarteo <d.sluijk@onestein.nl>
2024-11-20 11:51:45 +0100

    [IMP] Add mass_mailing_partner to mass_mailing_install

--- mass_mailing_install/__manifest__.py
+++ mass_mailing_install/__manifest__.py
@@ -6,6 +6,10 @@
     "category": "Email Marketing",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["mass_mailing", "mass_mailing_force_dedicated_server"],
+    "depends": [
+        "mass_mailing",
+        "mass_mailing_force_dedicated_server",
+        "mass_mailing_partner",
+    ],
     "bundle": True,
 }
