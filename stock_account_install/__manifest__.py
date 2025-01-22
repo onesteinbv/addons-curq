265708936268378b11f5482cc97d3d452a4eaf4c
tarteo <d.sluijk@onestein.nl>
2025-01-22 16:04:26 +0100

    [ADD] Bundle glue modules

--- stock_account_install/__manifest__.py
+++ stock_account_install/__manifest__.py
@@ -5,7 +5,12 @@
     "category": "Hidden",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["account_install", "stock_install", "account_move_line_stock_info"],
+    "depends": [
+        "account_install",
+        "stock_install",
+        "account_move_line_stock_info",
+        "account_move_line_purchase_info",
+    ],
     "auto_install": ["account_install", "stock_install"],
     "bundle": True,
 }
