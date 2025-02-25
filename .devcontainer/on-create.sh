5206d807244eed64ca9142be47e9e56929a2391f
tarteo <d.e.sluijk@gmail.com>
2025-02-25 14:30:32 +0000

    [WIP] Add devcontainer
    [UPD] Update devcontainer configuration: install additional dependencies and modify database settings
    
    [UPD] add git repository scan depth and update repos.yaml targets
    
    [FIX] devcontainer permissions
    
    [UPD] Remove unused volume declaration from Dockerfile
    
    [UPD] Normalize indentation in devcontainer.json

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+# Make data directory
+mkdir -p /odoo-data/filestore /odoo-data/sessions /odoo-data/logs
+chown -R vscode:vscode /odoo-data
+
+
+# Aggregate other repositories
+gitaggregate -c repos-dev.yaml
+
+# Install requirements
+pip install --no-cache-dir -r .repos/odoo/requirements.txt
+pip install --no-cache-dir -r requirements.txt
+pip install -e .repos/odoo
+
+# Install pre-commit hooks
+pre-commit install
+
+# Create config from template
+cat .devcontainer/odoo.cfg.tmpl | envsubst > odoo.cfg
