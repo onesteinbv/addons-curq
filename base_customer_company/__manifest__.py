a2cd3b2a03f1ee627b237675b0193d8d7662ea8d
Anjeel Haria <bytemeasap@gmail.com>
2025-08-06 13:59:57 +0530

    [MIG] base_customer_company: Migration to 18.0

---
+++ base_customer_company/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Customer Company",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "category": "Technical Settings",
+    "author": "Onestein",
+    "website": "https://www.onestein.eu",
+    "depends": ["base"],
+    "data": [],
+}
