a8045cd3efc878f656b372ef870d55151085ba40
Anjeel Haria <bytemeasap@gmail.com>
2025-08-06 14:01:16 +0530

    [MIG] base_customer_user: Migration to 18.0

---
+++ base_customer_user/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Customer User",
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "category": "Technical Settings",
+    "author": "Onestein",
+    "website": "https://www.onestein.eu",
+    "depends": [
+        "base",
+        "mail",
+        "mail_bot",
+    ],
+    "data": [
+        "data/res_users_data.xml",
+        "data/res_currency_data.xml",
+    ],
+}
