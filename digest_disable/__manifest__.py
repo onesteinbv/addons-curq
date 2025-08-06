6d2de24d8e7f1ec91127e83db62691a551486bf3
Anjeel Haria <bytemeasap@gmail.com>
2025-08-06 14:09:18 +0530

    [MIG] digest_disable: Migration to 18.0

---
+++ digest_disable/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Disable KPI Digests",
+    "author": "Onestein",
+    "website": "https://www.onestein.eu",
+    "category": "Marketing",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "digest",
+    ],
+    "data": ["data/ir_cron_data.xml"],
+}
