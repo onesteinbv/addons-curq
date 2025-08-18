94303913839da919351d932e73080cd6a2b627b5
Anjeel Haria <bytemeasap@gmail.com>
2025-08-18 18:11:21 +0530

    [MIG] stock_install: Migration to 18.0

---
+++ stock_install/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Inventory",
+    "summary": "Manage your stock and logistics activities",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Inventory",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock", "stock_dropshipping"],
+    "bundle": True,
+}
