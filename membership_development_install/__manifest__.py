b3cb8420a41e1bbe4cc52b3ba881aece21729d55
Anjeel Haria <bytemeasap@gmail.com>
2025-08-18 18:37:53 +0530

    [MIG] membership_development_install: Migration to 18.0

---
+++ membership_development_install/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Development Community",
+    "summary": "Integrates GitLab and GitHub to track repository activity and contributions in your community",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Membership",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "partner_cde",
+        "membership_activity",
+        "membership_activity_cde",
+        "membership_activity_gitlab",
+        "membership_activity_github",
+        "membership_nickname",
+    ],
+    "bundle": True,
+}
