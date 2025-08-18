e10668e2f37e1ad6c1c9c1da1bcd215e60ce9e0d
Anjeel Haria <bytemeasap@gmail.com>
2025-08-18 15:57:31 +0530

    [MIG] hr_install: Migration to 18.0

---
+++ hr_install/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017-2023 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Human Resources",
+    "summary": "Centralize employee information",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Human Resources",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "hr",
+        "hr_accessibility",
+        "hr_expense",
+        "hr_employee_firstname",
+        "hr_timesheet_sheet",
+        "hr_holidays",
+    ],
+    "bundle": True,
+}
