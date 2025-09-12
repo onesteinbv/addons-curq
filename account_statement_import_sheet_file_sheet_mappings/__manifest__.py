0a9433a142bd140b302bf7532e8ca3f67e606560
tarteo <d.e.sluijk@gmail.com>
2025-09-12 12:35:42 +0200

    Migration of account_statement_import_sheet_file_sheet_mappings to 18.0

---
+++ account_statement_import_sheet_file_sheet_mappings/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Bank Statement TXT/CSV/XLSX Import Sheet Mappings",
+    "summary": "Adds sheet mappings for bank statement imports",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.onestein.eu",
+    "author": "Onestein",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_statement_import_sheet_file",
+    ],
+    "data": [
+        "data/map_data.xml",
+    ],
+}
