080f9aaae48f39b08ba16b0f85f25d11692ef341
tarteo <d.e.sluijk@gmail.com>
2025-09-16 16:43:58 +0200

    Migration of l10n_nl_hr_install and l10n_nl_hr_recruitment to 18.0

---
+++ l10n_nl_hr_install/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Dutch Localization Human Resources",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Hidden",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_nl_install",
+        "hr_install",
+        "l10n_nl_hr_recruitment",
+        "l10n_nl_hr_expense",
+    ],
+    "auto_install": ["l10n_nl_install", "hr_install"],
+    "bundle": True,
+}
