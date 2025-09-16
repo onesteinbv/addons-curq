5e3d83e3659bd22dc9ef6647bec71dbc29995f5b
tarteo <d.e.sluijk@gmail.com>
2025-09-16 12:57:35 +0200

    Migration of stock_account_install to 18.0

---
+++ stock_account_install/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "WMS Accounting",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Hidden",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account_install",
+        "stock_install",
+        "account_move_line_stock_info",
+        "account_move_line_purchase_info",
+    ],
+    "auto_install": ["account_install", "stock_install"],
+    "bundle": True,
+}
