cd1cdbf183ab204684ac41a0337ce3138d519bf8
tarteo <d.e.sluijk@gmail.com>
2025-09-16 14:04:51 +0200

    Migration of membership_install to 18.0

---
+++ membership_install/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Membership Management",
+    "summary": "Efficiently manage memberships and administrative tasks",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Membership",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "data": ["security/security.xml"],
+    "depends": [
+        "membership",
+        "membership_contribution",
+        "membership_extension",
+        "membership_prorate",
+        "membership_subscription",
+        "membership_variable_period",
+        "membership_group",
+    ],
+    "bundle": True,
+}
