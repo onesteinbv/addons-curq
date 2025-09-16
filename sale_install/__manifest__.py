a5dcd35b173b8069d7e680ef651ecfbca7d83c1d
tarteo <d.e.sluijk@gmail.com>
2025-09-16 13:20:06 +0200

    sale_install migration to 18.0

---
+++ sale_install/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Sales Management",
+    "summary": "Sales process from quotations to invoices",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_management",
+        "sale_order_amount_to_invoice",
+        "delivery_sendcloud_oca",
+        "sale_operating_unit",
+        "sales_team_operating_unit",
+    ],
+    "bundle": True,
+}
