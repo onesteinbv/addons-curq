9c1d4a4125cca8034ed099dd6f781a53c84ddedc
tarteo <d.e.sluijk@gmail.com>
2025-09-16 16:00:04 +0200

    l10n_de_install migration to 18.0

---
+++ l10n_de_install/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Germany - Accounting & Localization",
+    "summary": "Fiscal and other localization features for German companies",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Localization",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_de",
+        "l10n_de_skr49",
+        "l10n_de_tax_statement",
+        "l10n_de_tax_statement_zm",
+    ],
+    "bundle": True,
+}
