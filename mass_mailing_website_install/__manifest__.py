8a05597239adfc8582bb31ef4175eccf0d81fed7
tarteo <d.e.sluijk@gmail.com>
2025-09-16 12:51:19 +0200

    Migration of mass_mailing_website_install

---
+++ mass_mailing_website_install/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Email Marketing Website",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Hidden",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mass_mailing_install",
+        "website_install",
+        "website_mailing_to_blogpost",
+    ],
+    "auto_install": ["mass_mailing_install", "website_install"],
+    "bundle": True,
+}
