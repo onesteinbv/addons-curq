043e283b4dde9b75cce92bd98182db4173fcfad1
tarteo <d.e.sluijk@gmail.com>
2025-09-17 15:47:08 +0200

    Migration of resource booking bundle to 18.0

---
+++ resource_booking_install/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Booking Management",
+    "summary": "Manage appointments and resource booking",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "data": ["security/security.xml"],
+    "depends": [
+        "resource_booking",
+        "sale_resource_booking",
+        "website_sale_resource_booking",
+    ],
+    "bundle": True,
+}
