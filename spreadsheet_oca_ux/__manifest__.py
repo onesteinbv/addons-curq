28ec870edf4d4e30190236656d501be7931a398c
tarteo <d.sluijk@onestein.nl>
2025-10-09 13:15:47 +0200

    Migrate spreadsheet_oca_ux to 18.0

---
+++ spreadsheet_oca_ux/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Spreadsheet OCA User Experience",
+    "author": "Onestein",
+    "website": "https://www.onestein.eu",
+    "category": "Technical",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["spreadsheet_oca", "spreadsheet_dashboard_oca"],
+    "data": [
+        "views/spreadsheet_view.xml",
+        "views/spreadsheet_dashboard_view.xml",
+    ],
+}
