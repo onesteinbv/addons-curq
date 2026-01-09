ae0d009903e07a838c410066b4507f03654e42dd
Anjeel Haria <bytemeasap@gmail.com>
2026-01-09 15:43:23 +0530

    [FIX] container_accessibility: Fix for issue coming up when restricted users edit website themes
    Updates

--- container_accessibility/models/ir_module_module.py
+++ container_accessibility/models/ir_module_module.py
@@ -4,26 +4,26 @@ from odoo import models
 class IrModuleModule(models.Model):
     _inherit = "ir.module.module"
 
-    def button_immediate_install(self):
+    def button_install(self):
         self_sudo = self
         if self.env.user.is_restricted_user() and not self.env.su:
             filtered = self._filtered_access("write")
             if filtered:
                 self_sudo = self.sudo()
-        return super(IrModuleModule, self_sudo).button_immediate_install()
+        return super(IrModuleModule, self_sudo).button_install()
 
-    def button_immediate_upgrade(self):
+    def button_upgrade(self):
         self_sudo = self
         if self.env.user.is_restricted_user() and not self.env.su:
             filtered = self._filtered_access("write")
             if filtered:
                 self_sudo = self.sudo()
-        return super(IrModuleModule, self_sudo).button_immediate_upgrade()
+        return super(IrModuleModule, self_sudo).button_upgrade()
 
-    def button_immediate_uninstall(self):
+    def button_uninstall(self):
         self_sudo = self
         if self.env.user.is_restricted_user() and not self.env.su:
             filtered = self._filtered_access("write")
             if filtered:
                 self_sudo = self.sudo()
-        return super(IrModuleModule, self_sudo).button_immediate_uninstall()
+        return super(IrModuleModule, self_sudo).button_uninstall()
