f941073b36f388d3a1b320de11a828e939b4ffb3
tarteo <d.sluijk@onestein.nl>
2026-01-16 16:51:03 +0100

    [ADD] New DMS bundle

---
+++ dms_install/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "DMS",
+    "summary": "Document Management System, organize your documents",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Document Management",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["dms", "web_editor_media_dialog_dms", "dms_field"],
+    "bundle": True,
+}
