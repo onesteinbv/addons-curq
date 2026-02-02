3f292f3cfc24cf5c8a91f2522e07cdfba63c70df
Anjeel Haria <bytemeasap@gmail.com>
2026-02-02 17:28:52 +0530

    [FIX] container_accessibility: Fix for access issue coming up while enabling/disabling incoming mail server

--- container_accessibility/models/fetchmail_server.py
+++ container_accessibility/models/fetchmail_server.py
@@ -1,7 +1,16 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FetchmailServer(models.Model):
     _inherit = "fetchmail.server"
 
     private = fields.Boolean(string="Is private server")
+
+    @api.model
+    def _update_cron(self):
+        sudo_self = self
+        if self.env.user.is_restricted_user() and self.env.user.has_group(
+            "base.group_system"
+        ):
+            sudo_self = self.sudo()
+        return super(FetchmailServer, sudo_self)._update_cron()
