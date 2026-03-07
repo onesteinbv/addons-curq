1d8a6b9c402af66e131f0b364bbb219efab710dd
tarteo <d.sluijk@onestein.nl>
2026-03-07 09:42:06 +0100

    Add options for incoming mail setup Quote ODOO_RC and DB_NAME args

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,16 @@
 
 USER_EMAIL=${USER_EMAIL:-$COMPANY_EMAIL}
 
-python /odoo/scripts/set_base_url.py -c $ODOO_RC -d $DB_NAME --log-level=error --domain "$DOMAIN"
+# TODO: Removed '-c "$ODOO_RC"', they are not necessary as Odoo will automatically use the ODOO_RC env variable
+
+python /odoo/scripts/set_base_url.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --domain "$DOMAIN"
 
 if [[ -n "$ADMIN_USER_PWD" && "$CHANGE_ADMIN_USER_PWD" == "true" ]]; then
-  python /odoo/scripts/change_password.py -c $ODOO_RC -d $DB_NAME --log-level=error --login admin --password "$ADMIN_USER_PWD"
+  python /odoo/scripts/change_password.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --login admin --password "$ADMIN_USER_PWD"
 fi
 
 if [[ -n "$SMTP_HOST" && "$SETUP_SMTP" == "true" ]]; then
-  python /odoo/scripts/setup_smtp.py -c $ODOO_RC -d $DB_NAME --log-level=error \
+  python /odoo/scripts/setup_smtp.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error \
     --host "$SMTP_HOST" \
     --user "$SMTP_USER" \
     --password "$SMTP_PASSWORD" \
@@ -18,29 +20,40 @@ if [[ -n "$SMTP_HOST" && "$SETUP_SMTP" == "true" ]]; then
 fi
 
 if [[ -n "$INCOMING_MAIL_SERVER" && "$SETUP_INCOMING_MAIL" == "true" ]]; then
-  if [[ $INCOMING_MAIL_CONFIRM == "true" ]]; then
-    python /odoo/scripts/setup_incoming_mail.py -c $ODOO_RC -d $DB_NAME --log-level=error --server "$INCOMING_MAIL_SERVER" --user "$INCOMING_MAIL_USER" --password "$INCOMING_MAIL_PASSWORD" --confirm
-  else
-    python /odoo/scripts/setup_incoming_mail.py -c $ODOO_RC -d $DB_NAME --log-level=error --server "$INCOMING_MAIL_SERVER" --user "$INCOMING_MAIL_USER" --password "$INCOMING_MAIL_PASSWORD"
+  flags=()
+  if [[ "$INCOMING_MAIL_CONFIRM" == "true" ]]; then
+    flags+=(--confirm)
   fi
-fi
+  if [[ -n "$INCOMING_MAIL_PORT" ]]; then
+    flags+=(--port "$INCOMING_MAIL_PORT")
+  fi
+  if [[ -n "$INCOMING_MAIL_SERVER_TYPE" ]]; then
+    flags+=(--server-type "$INCOMING_MAIL_SERVER_TYPE")
+  fi
+  if [[ "$INCOMING_MAIL_SSL" == "true" ]]; then
+    flags+=(--ssl)
+  else
+    flags+=(--no-ssl)
+  fi
 
+  python /odoo/scripts/setup_incoming_mail.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --server "$INCOMING_MAIL_SERVER" --user "$INCOMING_MAIL_USER" --password "$INCOMING_MAIL_PASSWORD" "${flags[@]}"
+fi
 
 python /odoo/scripts/localize.py
 
 # TODO: Remove capitalized True/False check, it's kept for backward compatibility but should be removed in favor of lowercase in the future
 if [[ -n "$UNINSTALL_MODULES" && ( "$UNINSTALL_MODULES" == "True" || "$UNINSTALL_MODULES" == "true" ) ]]; then
-  python /odoo/scripts/apply_modules.py -c $ODOO_RC -d $DB_NAME --log-level=error
+  python /odoo/scripts/apply_modules.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error
 else
-  python /odoo/scripts/apply_modules.py -c $ODOO_RC -d $DB_NAME --log-level=error --dry-run
+  python /odoo/scripts/apply_modules.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --dry-run
 fi
 
-python /odoo/scripts/uninstall_auto_install_modules.py -c $ODOO_RC -d $DB_NAME --log-level=error
+python /odoo/scripts/uninstall_auto_install_modules.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error
 
-python /odoo/scripts/remove_odoo_oauth_provider.py -c $ODOO_RC -d $DB_NAME --log-level=error
+python /odoo/scripts/remove_odoo_oauth_provider.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error
 
 if [[ -n "$KEYCLOAK_URL" ]]; then
-  python /odoo/scripts/setup_oauth.py -c $ODOO_RC -d $DB_NAME --log-level=error \
+  python /odoo/scripts/setup_oauth.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error \
     --url "$KEYCLOAK_URL" --realm "$KEYCLOAK_REALM" \
     --client-id "$KEYCLOAK_CLIENT_ID" --client-secret "$KEYCLOAK_CLIENT_SECRET" \
     --xml-id="__export__.__oauth_provider_onestein" \
@@ -48,7 +61,7 @@ if [[ -n "$KEYCLOAK_URL" ]]; then
     --template-user-id="base.user_admin"
 
   if [[ "${KEYCLOAK_RESELLER_REALM:-false}" != "false" ]]; then
-    python /odoo/scripts/setup_oauth.py -c $ODOO_RC -d $DB_NAME --log-level=error \
+    python /odoo/scripts/setup_oauth.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error \
       --url "$KEYCLOAK_URL" --realm "$KEYCLOAK_RESELLER_REALM" \
       --client-id "$KEYCLOAK_CLIENT_ID" --client-secret "$KEYCLOAK_RESELLER_CLIENT_SECRET" \
       --xml-id="__export__.__oauth_provider_reseller" \
@@ -59,9 +72,9 @@ if [[ -n "$KEYCLOAK_URL" ]]; then
 fi
 
 if [[ "$UPDATE_COMPANY" == "true" ]]; then
-  python /odoo/scripts/update_company.py -c $ODOO_RC -d $DB_NAME --log-level=error --name "$COMPANY_NAME" --email "$COMPANY_EMAIL" --coc "$COMPANY_COC" --city "$COMPANY_CITY" --zip "$COMPANY_ZIP" --street "$COMPANY_STREET"
+  python /odoo/scripts/update_company.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --name "$COMPANY_NAME" --email "$COMPANY_EMAIL" --coc "$COMPANY_COC" --city "$COMPANY_CITY" --zip "$COMPANY_ZIP" --street "$COMPANY_STREET"
 fi
 
 if [[ "$PREPARE_CUSTOMER_USER" == "true" ]]; then
-  python /odoo/scripts/prepare_customer_user.py -c $ODOO_RC -d $DB_NAME --log-level=error --email "$USER_EMAIL" --group-file /odoo/scripts/groups.txt
+  python /odoo/scripts/prepare_customer_user.py -c "$ODOO_RC" -d "$DB_NAME" --log-level=error --email "$USER_EMAIL" --group-file /odoo/scripts/groups.txt
 fi
