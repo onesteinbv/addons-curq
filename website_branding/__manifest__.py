99e70e97c523270985062c083945df40ebfcc210
tarteo <d.sluijk@onestein.nl>
2026-05-22 11:12:23 +0200

    [ADD] website_branding: Default CURQ colors in palletes and CURQ favicon

---
+++ website_branding/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "CURQ Branding in frontend",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Website",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "website",
+    ],
+    "assets": {
+        "web._assets_primary_variables": [
+            "website_branding/static/src/scss/primary_variables.scss"
+        ],
+    },
+    "data": [
+        "data/website_data.xml",
+    ],
+}
