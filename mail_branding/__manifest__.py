197e48972831b5775e6f364ce0767c88a6131a19
solomonprabu <solomon.workspace@gmail.com>
2026-05-25 15:09:48 +0530

    [Init] Initial commit module:mail_branding

---
+++ mail_branding/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Mail Branding",
+    "summary": "Mail Branding: Adapting to CURQ branding",
+    "author": "Onestein",
+    "website": "https://onestein.nl",
+    "category": "Mail",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+}
