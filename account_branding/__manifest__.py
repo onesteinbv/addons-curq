acfae0e370b27a95456a8c7e26d65b6fb87d9b2a
Dennis Sluijk <10028499+tarteo@users.noreply.github.com>
2026-06-05 09:57:15 +0200

    [ADD] account_branding: Remove mentions of Odoo in accounting (#385)

---
+++ account_branding/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Accounting Branding",
+    "author": "Onestein",
+    "category": "Accounting",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "templates/report_invoice.xml",
+    ],
+}
