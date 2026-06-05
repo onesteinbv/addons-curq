c00d1bea1ea121204717e5a108a715bac30761ec
tarteo <d.sluijk@onestein.nl>
2026-06-05 14:16:23 +0200

    [FIX] web_branding: Remove AppsMenuPreferences button

---
+++ web_branding/static/src/js/app_menu_preferences.esm.js
@@ -0,0 +1,8 @@
+import {xml} from "@odoo/owl";
+import {registry} from "@web/core/registry";
+import {apps_menu_preferences} from "@web_responsive/components/apps_menu/apps_menu_preferences.esm"; // eslint-disable-line no-unused-vars
+
+
+// Until https://github.com/OCA/web/pull/3569 is merged, we need to it this way.
+const AppsMenuPreferences = registry.category("systray").get("AppMenuTheme").Component;
+AppsMenuPreferences.template = xml``;
