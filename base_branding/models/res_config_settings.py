f008d8064d0c5bfd3d16b2876e7a8a0354732fa0
tarteo <d.sluijk@onestein.nl>
2026-06-09 11:04:16 +0200

    [IMP] base_branding: Add tests and refactor

--- base_branding/models/res_config_settings.py
+++ base_branding/models/res_config_settings.py
@@ -8,50 +8,80 @@ class ResConfigSettings(models.TransientModel):
 
     _inherit = "res.config.settings"
 
-    def _get_selectors_to_debrand(self):
+    def _get_selectors_to_brand(self):
         """
-        Returns a list of selectors (attribute names) to debrand.
+        Returns a list of selectors (xpath expressions) to brand.
         """
-        return ["string", "help", "title", "placeholder", "confirm"]
+        return ["//title", "//label", "//field", "//strong", "//button", "//setting"]
+
+    def _get_attributes_to_brand(self):
+        """
+        Returns a list of attributes to brand.
+        """
+        return [
+            "string",
+            "name",
+            "placeholder",
+            "confirm",
+            "title",
+            "help",
+        ]
+
+    def _brand_text(self, text):
+        """
+        Replaces occurrences of "Odoo" or "odoo" in the given text with "CURQ".
+        """
+
+        replacements = [
+            ("https://www.odoo.com", "https://curq.nl"),
+            ("Odoo", "CURQ"),
+            ("odoo", "CURQ"),
+        ]
+
+        if not text:
+            return text
+        for old, new in replacements:
+            text = text.replace(old, new)
+        return text
 
     @api.model
     def get_views(self, views, options=None):
         res = super().get_views(views, options=options)
 
-        if "form" in res["views"]:
-            xml_arch = etree.fromstring(res["views"]["form"]["arch"])
+        if "form" not in res["views"]:
+            return res
 
-            # 1. Strip out the documentation attribute from <setting> tags
-            for setting in xml_arch.xpath("//setting[@documentation]"):
-                setting.attrib.pop("documentation", None)
+        xml_arch = etree.fromstring(res["views"]["form"]["arch"])
 
-            # 2. Find and completely remove <widget name="documentation_link"/> elements
-            for widget in xml_arch.xpath("//widget[@name='documentation_link']"):
-                widget.getparent().remove(widget)
+        # Strip out the documentation attribute from <setting> tags
+        for setting in xml_arch.xpath("//setting[@documentation]"):
+            setting.attrib.pop("documentation", None)
 
-            # 3. Debrand XML
-            selectors_to_debrand = self._get_selectors_to_debrand()
-            for element in xml_arch.iter():
-                if element.text and ("Odoo" in element.text or "odoo" in element.text):
-                    element.text = element.text.replace("Odoo", "CURQ").replace(
-                        "odoo", "CURQ"
-                    )
-                if element.tail and ("Odoo" in element.tail or "odoo" in element.tail):
-                    element.tail = element.tail.replace("Odoo", "CURQ").replace(
-                        "odoo", "CURQ"
-                    )
-                if element.attrib:
-                    for attr, val in element.attrib.items():
-                        if attr in selectors_to_debrand and (
-                            "Odoo" in val or "odoo" in val
-                        ):
-                            element.attrib[attr] = val.replace("Odoo", "CURQ").replace(
-                                "odoo", "CURQ"
-                            )
+        # Find and completely remove <widget name="documentation_link"/> elements
+        for widget in xml_arch.xpath("//widget[@name='documentation_link']"):
+            widget.getparent().remove(widget)
 
-            # Assign the modified architecture string back to the view definition
-            res["views"]["form"]["arch"] = etree.tostring(
-                xml_arch, encoding="utf-8"
-            ).decode("utf-8")
+        # Brand XML
+        selectors_to_brand = self._get_selectors_to_brand()
+        for selector in selectors_to_brand:
+            for element in xml_arch.xpath(selector):
+                # Remove any occurrence of "Odoo" or "odoo" in the text content of the element
+                if element.text:
+                    element.text = self._brand_text(element.text)
 
+                # If the element has attributes, check if any of them are
+                # in the list of attributes to brand and replace "Odoo" or "odoo" in their values
+                if not element.attrib:
+                    continue
+
+                attributes_to_brand = self._get_attributes_to_brand()
+                for attr, val in element.attrib.items():
+                    if attr not in attributes_to_brand:
+                        continue
+                    element.attrib[attr] = self._brand_text(val)
+
+        # Assign the modified architecture string back to the view definition
+        res["views"]["form"]["arch"] = etree.tostring(
+            xml_arch, encoding="utf-8"
+        ).decode("utf-8")
         return res
