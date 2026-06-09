26c4d5e250f8b8fdb3f1c3dd58c6f99aaf0e882a
Dennis Sluijk <10028499+tarteo@users.noreply.github.com>
2026-06-09 11:37:04 +0200

    [ADD] auth_branding: open source authenticator app recommendations (#388)
    * [ADD] auth_branding
    
    * [FIX] web_branding: Remove documentation links to odoo.com
    
    * [FIX] web_branding: Remove AppsMenuPreferences button

---
+++ auth_branding/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Authentication Branding",
+    "summary": "Branding and Big Tech disengagement for authentication related views",
+    "category": "Extra Tools",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://onestein.nl",
+    "author": "Onestein",
+    "depends": [
+        "auth_totp",
+    ],
+    "data": [
+        "views/res_users_view.xml",
+        "wizards/auth_totp_wizard_view.xml",
+    ],
+    "auto_install": True,
+}
