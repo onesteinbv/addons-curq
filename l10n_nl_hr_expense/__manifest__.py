88ec1f09da0856c763637bae1e96aedb4b0d1a48
tarteo <d.sluijk@onestein.nl>
2026-07-03 15:22:53 +0200

    [FIX] l10n_nl_hr_expense: pre-commit issues

--- l10n_nl_hr_expense/__manifest__.py
+++ l10n_nl_hr_expense/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Dutch Localization Expenses",
     "author": "Onestein",
-    "website": "https://www.onestein.eu",
+    "website": "https://onestein.nl",
     "category": "Human Resources/Expenses",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
