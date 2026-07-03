6f7cc3f7430b128ecbb01df6a252082f61a8d0b8
tarteo <d.sluijk@onestein.nl>
2026-07-03 15:22:53 +0200

    [FIX] l10n_nl_hr_recruitment: pre-commit issues

--- l10n_nl_hr_recruitment/__manifest__.py
+++ l10n_nl_hr_recruitment/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Dutch Localization Recruitment",
     "author": "Onestein",
-    "website": "https://www.onestein.eu",
+    "website": "https://onestein.nl",
     "category": "Human Resources/Recruitment",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
