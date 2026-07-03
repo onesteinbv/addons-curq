7559c1eab564242d06d3022ad9f2ad745807c97c
tarteo <d.sluijk@onestein.nl>
2026-07-03 15:22:53 +0200

    [FIX] base_branding: pre-commit issues

--- base_branding/__manifest__.py
+++ base_branding/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Base Branding",
     "summary": "Base branding for Curq.",
     "author": "Onestein",
-    "website": "https://www.onestein.eu",
+    "website": "https://onestein.nl",
     "category": "Extra Tools",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
