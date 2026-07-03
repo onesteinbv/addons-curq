dfb408ef85cc75f00413ec10f839c3cd92b9ee7b
tarteo <d.sluijk@onestein.nl>
2026-07-03 15:22:53 +0200

    [FIX] sale_branding: pre-commit issues

--- sale_branding/__manifest__.py
+++ sale_branding/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Onestein",
-    "website": "https://onestein.eu",
+    "website": "https://onestein.nl",
     "depends": [
         "sale",
     ],
