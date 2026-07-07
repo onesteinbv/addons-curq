890f5b473508fb664c20e2ee86d2f9d6a34baaf9
tarteo <d.sluijk@onestein.nl>
2026-07-07 16:20:11 +0200

    [IMP] container_accessibility: Refactor private oauth provider to always have role

--- container_accessibility/tests/test_oauth.py
+++ container_accessibility/tests/test_oauth.py
@@ -4,7 +4,7 @@ from odoo.addons.base.tests.common import BaseCommon
 
 
 class TestOauth(BaseCommon):
-    def test_new_user_without_groups(self):
+    def test_new_user_with_private_provider(self):
         private_provider = self.env["auth.oauth.provider"].create(
             {
                 "private": True,
@@ -32,6 +32,9 @@ class TestOauth(BaseCommon):
         self.assertTrue(new_user.has_group("base.group_portal"))
         self.assertFalse(new_user.has_group("base.group_system"))
         self.assertFalse(new_user.has_group("container_accessibility.group_restricted"))
+        self.assertEqual(
+            new_user.role_id, self.ref("container_accessibility.role_portal")
+        )
 
     def test_private_provider_without_role(self):
         with self.assertRaises(ValidationError):
