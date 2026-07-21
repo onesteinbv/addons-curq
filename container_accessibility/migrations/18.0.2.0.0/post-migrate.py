f73b87dad2bc1c1606175b3221993db8098e69a9
tarteo <d.sluijk@onestein.nl>
2026-07-21 10:11:16 +0200

    [FIX] container_accessibility: Fix post-migrate script

--- container_accessibility/migrations/18.0.2.0.0/post-migrate.py
+++ container_accessibility/migrations/18.0.2.0.0/post-migrate.py
@@ -12,11 +12,9 @@ def migrate(cr, version):
     portal_users = env["res.users"].search([("share", "=", True)])
     portal_users.write({"role_id": role_guest.id})
 
-    # Assign restricted users without private oauth to the manager role
+    # Assign restricted users to the manager role
     normal_users = env["res.users"].search(
         [
-            ("share", "=", False),
-            ("oauth_provider_id", "=", False),
             ("groups_id", "in", group_restricted.ids),
         ]
     )
@@ -25,10 +23,8 @@ def migrate(cr, version):
     # Assign restricted users with private oauth to the administrator role
     admin_users = env["res.users"].search(
         [
-            ("share", "=", False),
             ("oauth_provider_id", "!=", False),
             ("oauth_provider_id.private", "=", True),
-            ("groups_id", "in", group_restricted.ids),
         ]
     )
     admin_users.write({"role_id": role_administrator.id})
