bb7bb03337b778cc00b3d6f7f873da429b7a6308
Anjeel Haria <bytemeasap@gmail.com>
2026-09-03 15:33:40 +0530

    [UPD] base_module_bundle: Add category Bundles to Apps view

--- base_module_bundle/__manifest__.py
+++ base_module_bundle/__manifest__.py
@@ -4,7 +4,8 @@
     "version": "18.0.1.0.0",
     "author": "Onestein",
     "website": "https://onestein.nl",
-    "depends": ["base_setup"],
-    "data": [],
+    "depends": ["base_setup", "base_import_module"],
+    "data": ["views/ir_module_module_views.xml"],
     "license": "AGPL-3",
+    "post_init_hook": "post_init_hook",
 }
